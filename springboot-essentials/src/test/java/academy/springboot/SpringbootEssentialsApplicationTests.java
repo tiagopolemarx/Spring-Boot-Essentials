57981f295a43c5add0f4bdf95e5141ee6c80af9d
unknown <tiago.polemarxgroup@gmail.com>
2021-02-10 02:11:39 -0300

    Initial commit

---
+++ springboot-essentials/src/test/java/academy/springboot/SpringbootEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
